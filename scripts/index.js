8e924c32c279057c71395ba27bc8d2c090706f8e
A1zen-Sosuk3 <aoandopfor@gmail.com>
2023-02-19 13:50:25 +0100

    Added the test animation at index.html page,added content to addition.html, changed the animation of typing text at index.html, little changes in main content etc.

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-//typing text
+"use strict"
 
 let typed = new Typed('#typed', {
     typeSpeed: 100,
@@ -6,4 +6,16 @@ let typed = new Typed('#typed', {
     startDelay: 500,
     loop: true,
     stringsElement: '#typed-strings'
+});
+
+VANTA.NET({
+    el: "#wrapper",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    backgroundColor: 0x0
 });
